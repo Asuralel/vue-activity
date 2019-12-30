60080b4985ea7f4104e6e98671f592a5d7d441c9
李发强 <lifaqiang007@126.com>
2019-12-30 10:51:13 +0800

    init:初始化项目

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style lang="less">
+@import "./style/reset.less";
+@import "./style/variable.less";
+@import '~vux/src/styles/reset.less';
+@import '~vux/src/styles/1px.less';
+</style>
